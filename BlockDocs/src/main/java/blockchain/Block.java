6ae12a422a294efdac8411740d6d22560e9adf8c
김형기 <khk211113@naver.com>
2022-12-06 01:26:54 +0900

    chore: project init

---
+++ BlockDocs/src/main/java/blockchain/Block.java
@@ -0,0 +1,12 @@
+package blockchain;
+
+public class Block {
+    private long timestamp;
+    private String hash;
+    private String previousHash;
+    private int nonce;
+
+    public Block() {
+
+    }
+}
